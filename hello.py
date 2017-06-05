f488a8c8bf6c41a7f50ca0664d1f88b18f82737f
Gary Smith <gary.smith@suse.com>
2017-06-05 16:20:51 -0700

    Add /version
    Change-Id: I217c0c8d68ae90304337a1e65e37574406b529a4

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
 from flask import Flask
 import time
+import pbr.version
+
 app = Flask(__name__)
+
+
+@app.route("/version")
+def version():
+    version_info = pbr.version.VersionInfo('ardana-server')
+    return version_info.version_string()
 
 
 @app.route("/heartbeat")
