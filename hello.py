06abd44d47282898682a9d28368d85ce7ee21066
Gary Smith <gary.smith@suse.com>
2017-06-12 08:15:33 -0700

    Add /playbooks, use debug mode
    Change-Id: If686fc8873e2df31d44acf1be08cb85922c2fe5c

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 from flask import Flask
-import time
+from flask import jsonify
 import pbr.version
+import re
+import os
+import time
 
 app = Flask(__name__)
 
@@ -14,8 +17,26 @@ def version():
 @app.route("/heartbeat")
 def heartbeat():
     # return ms since epoch
-    return str(int(1000 * time.time()))
+    return jsonify(int(1000 * time.time()))
+
+
+@app.route("/playbooks")
+def playbooks():
+    # TODO: read playbooks_dir from config file
+    playbooks_dir = "/data/home/dev/scratch/ansible/next/hos/ansible"
+
+    yml_re = re.compile(r'\.yml$')
+
+    playbooks = {'config-processor-run',
+                 'config-processor-clean',
+                 'ready-deployment'}
+    for filename in os.listdir(playbooks_dir):
+        if filename[0] != '_' and yml_re.search(filename):
+            # Strip off extension
+            playbooks.add(filename[:-4])
+
+    return jsonify(sorted(playbooks))
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
