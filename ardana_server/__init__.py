736d69c7c1f1b1e4681d0466f0841c4e1400cc62
Gary Smith <gary.smith@suse.com>
2017-06-27 10:55:02 -0700

    Change async mode to make it work

--- ardana_server/__init__.py
+++ ardana_server/__init__.py
@@ -4,7 +4,7 @@ from flask_socketio import SocketIO
 #   thread to pause while the background thread runs. The "threading"
 #   option works fine in dev mode, but only supports long polling
 #   (not WebSockets).
-socketio = SocketIO() # async_mode="threading")
+socketio = SocketIO(async_mode="threading")
 
 # Import any modules that refer to socketio here (after socketio has been
 # created)
