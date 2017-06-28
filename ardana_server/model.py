ef7f5ddd26795f3458d98363889dd2a67599935f
Gary Smith <gary.smith@suse.com>
2017-06-28 10:21:15 -0700

    Add support for objects
    Change-Id: I27bef2521c4e94e946cd3a551bb0838d6b3fb904

--- ardana_server/model.py
+++ ardana_server/model.py
@@ -3,17 +3,18 @@ import collections
 import logging
 import os
 import re
-import time
 import yaml
 
 LOG = logging.getLogger(__name__)
 
 # os.path.expanduser("~/dev/helion/my_cloud/definition")
-MODEL_DIR = os.path.join(os.path.dirname(__file__), '..', 'model')
+MODEL_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__), '..',
+                             'model'))
 
 CLOUD_CONFIG = "cloudConfig.yml"
 
 bp = Blueprint('model', __name__)
+
 
 @bp.route("/v2/model")
 def get_model():
@@ -61,8 +62,8 @@ def read_model(model_dir):
     with open(cloud_config_file) as f:
         try:
             doc = yaml.safe_load(f)
-        except yaml.YAMLError as e:
-            logger.exception("Invalid yaml file")
+        except yaml.YAMLError:
+            LOG.exception("Invalid yaml file")
 
     if not doc:
         return model
@@ -78,46 +79,48 @@ def read_model(model_dir):
         raise 'Cloud config error: no name specified'
 
     relname = CLOUD_CONFIG
-    model['fileInfo'] = {'configFile': cloud_config_file,
-                         'directory': model_dir,
-                         'files': [ relname ],
-                         'sections': collections.defaultdict(list),
-                         'fileSectionMap': collections.defaultdict(list),
-                         'mtime': os.stat(cloud_config_file).st_mtime,
-                         '_object_data': collections.defaultdict(list),
-                        }
+    model['fileInfo'] = {
+        'configFile': cloud_config_file,
+        'directory': model_dir,
+        'files': [relname],
+        'sections': collections.defaultdict(list),
+        'fileSectionMap': collections.defaultdict(list),
+        'mtime': os.stat(cloud_config_file).st_mtime,
+        '_object_data': collections.defaultdict(list),
+    }
     model['inputModel'] = {}
 
     add_to_model(model, doc, relname)
 
     yml_re = re.compile(r'\.yml$')
-    try:
-        for root, dirs, files in os.walk(model_dir):
-            for file in files:
-                relname = os.path.relpath(os.path.join(root, file), model_dir)
-                if file != CLOUD_CONFIG and yml_re.search(file):
-                    model['fileInfo']['files'].append(relname)
-                elif file.startswith('README'):
-                    ext = file[7:]
-                    with open(file) as f:
-                        lines = f.readlines()
-                    model['readme'][ext] = ''.join(lines)
+    for root, dirs, files in os.walk(model_dir):
+        for file in files:
+            # avoid processing top-leve cloud config again
+            if file == CLOUD_CONFIG:
+                continue
 
-                filename = os.path.join(root, file)
-                with open(filename) as f:
-                    try:
-                        doc = yaml.safe_load(f)
-                        add_to_model(model, doc, relname)
-                    except yaml.YAMLError as e:
-                        logger.exception("Invalid yaml file")
+            relname = os.path.relpath(os.path.join(root, file), model_dir)
+            if yml_re.search(file):
+                model['fileInfo']['files'].append(relname)
+            elif file.startswith('README'):
+                ext = file[7:]
+                with open(file) as f:
+                    lines = f.readlines()
+                model['readme'][ext] = ''.join(lines)
 
-    except OSError:
-        LOG.warning("Playbooks directory %s doesn't exist. This could indicate"
-                    " that the ready_deployment playbook hasn't been run yet. "
-                    "The list of playbooks available will be reduced",
-                    PLAYBOOKS_DIR)
+            filename = os.path.join(root, file)
+            with open(filename) as f:
+                try:
+                    doc = yaml.safe_load(f)
+                    add_to_model(model, doc, relname)
+                except yaml.YAMLError:
+                    LOG.exception("Invalid yaml file")
+
+    update_objects(model)
+    del model['fileInfo']['_object_data']
 
     return model
+
 
 def add_to_model(model, doc, relname):
 
@@ -129,30 +132,62 @@ def add_to_model(model, doc, relname):
         if isinstance(value, list):
             key_field = get_key_field(value[0])
             mapping = {
-                    'keyField': key_field,
-                    'type': 'array',
-                    section: [e[key_field] for e in value],
+                'keyField': key_field,
+                'type': 'array',
+                section: [e[key_field] for e in value],
             }
             model['fileInfo']['fileSectionMap'][relname].append(mapping)
-
-            if section not in model['inputModel']:
-                model['inputModel'][section] = []
-            model['inputModel'][section].append(value)
 
         elif isinstance(value, dict) and section != 'product':
             # product, which always is the dict {'version':2}, is handled as a
             # primitive
-            obj = {'index': index, relname: value}
-            model['fileInfo']['_object_data'][section] = obj
+            # Track where objects are, but don't add them to the fileSectionMap
+            # yet
+            obj = {'index': index,
+                   'file': relname,
+                   'data': value}
+            model['fileInfo']['_object_data'][section].append(obj)
             index += 1
 
+        else:  # primitive or product
+            model['fileInfo']['fileSectionMap'][relname].append(section)
+
+        if isinstance(value, list):
+            if section not in model['inputModel']:
+                model['inputModel'][section] = []
+            model['inputModel'][section].extend(value)
+        else:
             if section not in model['inputModel']:
                 model['inputModel'][section] = {}
             model['inputModel'][section].update(value)
 
-        else:  # primitive
-            model['fileInfo']['fileSectionMap'][relname].append(section)
-            model['inputModel'][section] = value
+
+def update_objects(model):
+    """
+    Update fileSectionMaps for files that contain an objects section
+    """
+    for section, obj_list in model['fileInfo']['_object_data'].iteritems():
+        if len(obj_list) > 1:
+            # pass-through is the only section supported in multiple files
+            if section != 'pass-through':
+                raise Exception('The section %s has been found in multiple '
+                                'files, which is not currently supported' %
+                                section)
+
+            # FIXME(gary): Handling pass-through sections is borked.  This has
+            # to be tested with one of the models that has them
+            for obj in obj_list:
+                relname = obj['file']
+
+                mapping = {
+                    'type': 'object',
+                }
+                model['fileInfo']['fileSectionMap'][relname].append(mapping)
+        else:
+            relname = obj_list[0]['file']
+            index = obj_list[0]['index']
+            model['fileInfo']['fileSectionMap'][relname].insert(index, section)
+
 
 def write_model(model_dir):
     pass
