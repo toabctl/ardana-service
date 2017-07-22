c1c0c2404507c5d413f9aee5b5f10437aff53039
Gary Smith <gary.smith@suse.com>
2017-07-21 17:52:39 -0700

    Add some write tests
    Change-Id: I5025b2848f23f6d8d33a2507fcb2c6305c4eb0ce

--- ardana_service/tests/test_model.py
+++ ardana_service/tests/test_model.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import unittest
 import yaml
@@ -28,3 +29,36 @@ class MyTest(unittest.TestCase):
         model_dir = os.path.join(os.path.dirname(__file__), 'test_data_invalid', 'doesnotexist')
         with self.assertRaises(IOError):
             model.read_model(model_dir)
+
+
+    def test_no_changes(self):
+
+        data = model.read_model(self.model_dir)
+        changes = model.write_model(data, self.model_dir, dry_run=True)
+
+        changed_files = [k for k,v in changes.iteritems()
+                        if v.get('changed', False)]
+        self.assertEquals(0, len(changed_files), "No files should have changed")
+
+    def test_add_servers(self):
+
+        num_to_add = 5
+
+        data = model.read_model(self.model_dir)
+
+        before_len = len(data['inputModel']['servers'])
+        server = data['inputModel']['servers'][-1:][0]
+        for i in range(0, num_to_add):
+            clone = copy.deepcopy(server)
+            clone['id'] = 'server-%s' % i
+            data['inputModel']['servers'].append(clone)
+
+        changes = model.write_model(data, self.model_dir, dry_run=True)
+        changed_files = [k for k,v in changes.iteritems()
+                        if v.get('changed', False)]
+
+        self.assertEquals(1, len(changed_files), "Only should have written one file")
+        self.assertEquals(changed_files[0], "data/servers.yml",
+            "Only should have written servers file")
+        after_len = len(changes[changed_files[0]]['data']['servers'])
+        self.assertEquals(before_len+num_to_add, after_len, "Should have written %s more" % num_to_add)
